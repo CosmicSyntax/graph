e842c301f12b3bb79b5bcf3134d0c7f69918f5e4
Daniel Choi <choi.mina@gmail.com>
2020-01-20 17:50:07 +0000

    Implemented display trait for struct Node

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
-/// This is a struct used to represent each node
+/// This is a struct used to represent each node. The data it can house
+/// can be currently be anything.
+use std::fmt;
+
 pub struct Node<T> {
     pub data: T,
-    weight: Option<u8>,
+    weight: u8,
     next: Option<Box<Node<T>>>,
 }
 
 pub struct Graph<T>(Vec<Node<T>>);
 
 impl<T> Graph<T> {
-    pub fn new(data: T, weight: Option<u8>) -> Vec<Node<T>> {
+    pub fn new(data: T, weight: u8) -> Vec<Node<T>> {
         vec![
             Node {
                 data,
@@ -18,7 +21,7 @@ impl<T> Graph<T> {
         ]
     }
 
-    pub fn with_capacity(data: T, weight: Option<u8>, capacity: usize)
+    pub fn with_capacity(data: T, weight: u8, capacity: usize)
     -> Vec<Node<T>>
     {
         let mut node: Vec<Node<T>> = Vec::with_capacity(capacity);
@@ -29,4 +32,27 @@ impl<T> Graph<T> {
         });
         node
     }
+
+    pub fn add(&self, data: T, weight: u8) {
+        todo!();
+    }
+
+}
+
+impl<T> fmt::Display for Node<T> 
+where T: fmt::Display
+{
+
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut message = String::new();
+        message.push_str(&format!("Data:\t{}\n", self.data));
+        message.push_str(&format!("Weight:\t{}\n", self.weight));
+        if let Some(x) = &(self.next) {
+            message.push_str(&format!("Next:\t{}\n", x));
+        } else {
+            message.push_str("Next:\tNo Data");
+        }
+        write!(f, "{}", message)
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use graph::*;
 
 fn main() {
-    let a = Graph::new("hello", Some(2));
-    let b = Graph::with_capacity("world!", Some(5), 50); 
+    let a = Graph::new("hello", 2);
+    let b = Graph::with_capacity("world!", 5, 50); 
 
     println!("{}", a[0].data);
     println!("{}", b.capacity());
+    println!("{}", a[0]);
 }
